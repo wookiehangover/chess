2ec2c781fef46d2386b6411ad2076ac274ec4283
unknown <samuel.breed@gmail.com>
2019-01-21 20:08:26 -0800

    print possible moves

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const { Game } = require('./game')
 
+/*
+// Knight Test Case
+const { wN, bN } = require('./pieces')
+let game = new Game([
+  bN('b8'),
+  wN('g1')
+])
+game.move('g1', 'e2')
+game.move('b8', 'c6')
+game.move('e2', 'f4')
+game.move('c6', 'd4')
+game.move('f4', 'e6')
+game.move('d4', 'e6')
+*/
+
 let game = new Game()
 
 game.move('e2', 'e4')
 game.move('e7', 'e5')
-game.move('f1', 'b5')
-game.move('c7', 'c6')
-game.move('d1', 'h5')
+game.move('g1', 'f3')
+game.move('b8', 'c6')
+game.move('g2', 'g3')
+game.move('d7', 'd5')
+game.move('e4', 'd5')
+
+const printMoves = (board, position) => {
+  const piece = board.getPiece(position)
+  const moves = piece && piece.validMoves(board)
+  if (piece && moves.length > 0) {
+    console.log(`${piece.toChar()} ${piece.position} =>`, moves.join(', '))
+  }
+}
+
+console.log(`Next move: ${game.nextMove}`)
+console.log('\nPossible moves:')
+game.pieces.forEach((piece) => {
+  if (piece.color === game.nextMove) {
+    printMoves(game.board, piece.position)
+  }
+})
+
+// game.move('f1', 'b5')
+// game.move('c7', 'c6')
+// game.move('d1', 'h5')
+// game.move('c6', 'b5')
+// game.move('h5', 'e5')
