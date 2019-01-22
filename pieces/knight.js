48cc02ab13776598c5d753561a472b4b994d9d3c
unknown <samuel.breed@gmail.com>
2019-01-21 16:09:27 -0800

    knight implementation

--- pieces/knight.js
+++ pieces/knight.js
@@ -1,6 +1,42 @@
 const Piece = require('./piece')
+const { coordsFromPosition, getVectors } = require('../utils')
+
+const absProduct = (v) => Math.abs(v[0] * v[1])
 
 class Night extends Piece {
+  move (board, position) {
+    const nextCoords = coordsFromPosition(position)
+    let isValid = false
+
+    // console.log('===> Move:', this.position, position); debugger
+
+    // knights can move 2 spaces in a {row, col}, followed by 1 spaces {col, row}
+    const [ rowVector, colVector ] = getVectors(this.coords, nextCoords)
+    const col = absProduct(colVector)
+    const row = absProduct(rowVector)
+
+    if ((col === 1 && row === 2) ||
+        (col === 2 && row === 1)) {
+      isValid = true
+    }
+
+    const piece = board.getPiece(position)
+    if (piece !== false && isValid) {
+      if (piece.color === this.color) {
+        isValid = false
+      } else {
+        isValid = true
+        this.capture(piece)
+      }
+    }
+
+    return isValid
+  }
+
+  get fullName () {
+    return 'Knight'
+  }
+
   toChar () {
     return this.color === 'white' ? '\u2658' : '\u265e'
   }
